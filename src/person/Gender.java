3edbf9a665d1d3e8de872224807d6af0d83169cb
Joseph James <josephjames@Josephs-MacBook-Air.local>
2021-06-19 20:37:52 +0530

    added person implementation

---
+++ src/person/Gender.java
@@ -0,0 +1,19 @@
+package person;
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * @author JITHIN GEORGE
+ *
+ */
+public enum Gender {
+	//This signifies the person is Male
+	MALE,
+	//This signifies the person is Female 
+	FEMALE,
+	//This signifies the person is Transgender
+	TRANSGENDER;
+
+}
