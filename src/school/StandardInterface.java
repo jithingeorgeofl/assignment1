64df10df158eae6de80cbcf5cbbc955debd06b6f
Joseph James <josephjames@Josephs-MacBook-Air.local>
2021-06-24 23:06:59 +0530

    test

---
+++ src/school/StandardInterface.java
@@ -0,0 +1,13 @@
+package school;
+
+public interface StandardInterface {
+	
+	Integer getFirstTermMarks();
+	
+	Integer getSecondTermMarks();
+	
+	Boolean isCourseOnGoing();
+	
+	Grades getGrade();
+
+}
---
+++ src/tests/BaseStandardTest.java
@@ -0,0 +1,63 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import school.BaseStandard;
+import school.Grades;
+
+class BaseStandardTest {
+	
+	private BaseStandard standard;
+	
+	@BeforeEach
+	public void setUp() {
+		
+		 standard = new BaseStandard(
+				 
+				 true,
+				 20,
+				 30,
+				 Grades.STD1);
+		 
+	}
+	
+	@Test
+	void testForIsCourseOngoing() {
+		
+		standard.isCourseOnGoing();
+		
+		assertTrue(
+				standard.isCourseOnGoing() == true);
+	}
+	
+	@Test
+	void testForFirstTermMarks() {
+		
+		standard.getFirstTermMarks();
+		
+		assertTrue(
+				standard.getFirstTermMarks().equals(20));
+	}
+	
+	@Test
+	void testForSecondTermMarks() {
+		
+		standard.getSecondTermMarks();
+		
+		assertTrue(
+				standard.getSecondTermMarks().equals(30));
+		
+	}
+	@Test
+	void testForGrades() {
+		
+		standard.getGrade();
+		
+		assertTrue(
+				standard.getGrade().equals(Grades.STD1));
+	}
+				
+}
